7fcd0871e8ecdb107895a243dd6900d032c8ca4c
Peter <peter.binder.10@gmail.com>
2019-03-11 15:35:51 -0500

    add cursor class and join game function

--- js/pete-sketch.js
+++ js/pete-sketch.js
@@ -14,10 +14,44 @@ function setup() {
 
 function draw() {
     background(0);
-   // console.log(sketchWidth);
-    //console.log(sketchHeight);
+
+    
 }
 
+function Cursor(cursorW, cursorH, cursorColor, cursorX, cursorY) {
+    this.name;
+    this.type = type;
+    this.cursorW = cursorW;
+    this.cursorH = cursorH;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.cursorX = cursorX;
+    this.cursorY = cursorY;
+    
+    this.update = function() {
+    
+    }
+}
+
+//create a new player to be added to the sketch
+function createPlayers() {
+    console.log('Player created');
+    players = new Array();
+
+    for(var i = 0; i < players.length; i++) {
+
+        //create a new cursor
+        players[i] = new Cursor(30, 30, "red", 100, 120 + (40*(i+1)))
+
+        //add player to the array
+        players[i].name = roomMembers[i];
+
+        console.log(players[i].name);
+    }
+}
+
+
+//resize canvas to the current height/width of the div
 function resetCanvasSize() {
     sketchWidth = canvasDiv.offsetWidth;
     sketchHeight = canvasDiv.offsetHeight;
